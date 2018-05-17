bf2634b89c70fb9aa1f22f2829e665f1f341c372
kz56cd <kz56cd@gmail.com>
2018-05-17 15:55:55 +0900

    Add swiftgen run script

---
+++ PreMaterialComponents/TopViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TopViewController.swift
+//  PreMaterialComponents
+//
+//  Created by Masakazu Sano on 2018/05/17.
+//  Copyright © 2018年 Masakazu Sano. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
